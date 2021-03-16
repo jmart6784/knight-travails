fbe56fdedd775c8ede5d3f85e1936fce0289ccfd
jmart6784 <jmartinez6784@yahoo.com>
2021-03-15 23:42:02 -0700

    Added Board class with pieces as nodes

--- knight.rb
+++ knight.rb
@@ -7,3 +7,38 @@ class Node
     @parent = @parent
   end
 end
+
+class Board
+  attr_accessor :board_nodes
+
+  def initialize
+    @board_nodes = place_nodes
+  end
+
+  def place_nodes
+    x = 0
+    y = 0
+    nodes = []
+
+    64.times do
+      id = [x, y]
+
+      nodes << Node.new(id)
+
+      if y < 7
+        y += 1
+      else
+        x += 1
+        y = 0
+      end
+    end
+    nodes
+  end
+
+
+
+end
+
+b = Board.new
+
+# puts b.place_nodes.inspect
